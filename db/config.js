ca68cfa8c26e8cd8b76d16a63daf19b4c820c532
e__popova <e.popova@dunice.net>
2024-03-04 11:23:05 +0300

    added ConfigModule.forRoot to .sequelizerc

--- db/config.js
+++ db/config.js
@@ -4,6 +4,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
     dialect: process.env.DB_DIALECT,
   },
 };
