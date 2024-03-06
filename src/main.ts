d6aea61deb7534fd83cdacafe64e367d9c2bef02
e__popova <e.popova@dunice.net>
2024-03-06 10:04:05 +0300

    added trim transform

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,13 @@
 import { NestFactory } from '@nestjs/core';
-import { BadRequestException, ValidationPipe } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 
 import { AppModule } from './app.module';
-import { ValidationError } from 'class-validator';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
-      exceptionFactory: (errors: ValidationError[]) => {
-        const result = errors.map((error) => ({
-          property: error.property,
-          message: error.constraints[Object.keys(error.constraints)[0]],
-        }));
-        return new BadRequestException(result);
-      },
-      stopAtFirstError: true,
+      transform: true,
     }),
   );
   await app.listen(process.env.PORT);
--- src/todo/dto/create-task.dto.ts
+++ src/todo/dto/create-task.dto.ts
@@ -1,9 +1,9 @@
 import { Transform, TransformFnParams } from 'class-transformer';
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateTaskDto {
   @IsNotEmpty({ message: 'Task should not be empty' })
-  // @IsString({ message: 'Task should be a string' })
-  @Transform(({ value }: TransformFnParams) => console.log('value :>> ', value))
+  @IsString({ message: 'Task should be a string' })
+  @Transform(({ value }: TransformFnParams) => value.trim())
   name: string;
 }
